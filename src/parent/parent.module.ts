13edc761952159fdc55a7aa582775c9ddfb7ebf1
Jolomi Dudu <jollofdudu@gmail.com>
2026-09-08 12:04:28 +0200

    Import AuthModule into ParentModule

--- src/parent/parent.module.ts
+++ src/parent/parent.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ParentService } from './parent.service.js';
 import { ParentController } from './parent.controller.js';
 import { PrismaModule } from '../prisma/prisma.module.js';
+import { AuthModule } from '../auth/auth.module.js';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, AuthModule],
   controllers: [ParentController],
   providers: [ParentService],
 })
